12c34b3430b8ce7353a6794644090af1b745dd3b
Harshitanetam <75417797+Harshitanetam@users.noreply.github.com>
2021-01-27 22:14:38 +0530

    Update drops.js

--- drops.js
+++ drops.js
@@ -7,8 +7,8 @@ class Drops {
         this.r=r;
       
         this.body = Bodies.circle(x, y, this.r,options);       
-        //this.color=color(random(0, 255), random(0, 255), random(0, 255));
-        World.add(world, this.body);
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+       World.add(world, this.body);
 
     }
     display() {
@@ -21,7 +21,7 @@ class Drops {
         rotate(angle);
         //imageMode(CENTER);
         noStroke();
-        
+        fill(this.color);
         ellipseMode(RADIUS);
         ellipse(0, 0, this.r,this.r);
         pop();
